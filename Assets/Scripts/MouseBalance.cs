dd9a6ef2f6355bd07475f7eb510231a51447740b
worm00111 <vdrumsta@gmail.com>
2016-10-30 16:14:22 +0000

    Added Unity Folder
    Launch this folder through unity projects

---
+++ Assets/Scripts/MouseBalance.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseBalance : MonoBehaviour
+{
+
+    public float rotationSpeed;             // How sensitive the torque is applied
+    public float rotationSpeedLimit;        // Fastest speed the body can rotate at
+    public float rotationDeccelerationRate; // If no input is given, how fast players balancing input looses speed
+
+    private float rotationAcceleration;     // How much torque it's applying
+    private float accelerationLimit;        // Max torque acceleration
+    private float rotationDecceleration;
+
+    // Use this for initialization
+    void Start()
+    {
+        rotationAcceleration = 0;
+        accelerationLimit = 10;
+        rotationDecceleration = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+        applyRotation();
+    }
+
+    private void applyRotation()
+    {
+        rotationAcceleration += Input.GetAxis("Horizontal");
+        //rotationAcceleration += Input.GetAxis("Horizontal");
+        rotationAcceleration = Mathf.Clamp(rotationAcceleration, -accelerationLimit, accelerationLimit);
+
+        GetComponent<Rigidbody2D>().AddTorque(rotationAcceleration * rotationSpeed * Time.deltaTime);
+        GetComponent<Rigidbody2D>().angularVelocity = Mathf.Clamp(GetComponent<Rigidbody2D>().angularVelocity, -rotationSpeedLimit, rotationSpeedLimit);
+
+        if (Input.GetAxis("Horizontal") == 0) // if mouse is not moved, deccelerate it
+        //if (Input.GetAxis("Horizontal") == 0) // if mouse is not moved, deccelerate it
+        {
+            /*
+            rotationAcceleration = Mathf.Lerp(rotationAcceleration, 0, 0.05f);
+            Debug.Log(rotationAcceleration);
+            //*/
+            if (rotationAcceleration < -accelerationLimit * 0.1)    // if the acceleration is higher than 10% of the limit
+            {
+                rotationAcceleration += rotationDecceleration;
+            }
+            else if (rotationAcceleration > accelerationLimit * 0.1)
+            {
+                rotationAcceleration -= rotationDecceleration;
+            }
+            rotationDecceleration += rotationDeccelerationRate;
+        }
+        else
+        {
+            rotationDecceleration = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed; // How fast the player moves based on the angle
+    public MouseBalance bodyMouseBalanceScript;
+    public GameObject bodyGameObject;
+    public Transform bodyTransform;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float bodyRotation = bodyTransform.eulerAngles.z;
+        Debug.Log(bodyRotation);
+        if (bodyRotation > 180)
+        {
+            bodyRotation = 360 - bodyRotation;
+        }
+        else
+        {
+            bodyRotation = -(bodyRotation);
+        }
+        rb.AddForce(new Vector2(bodyRotation/82 * movementSpeed, 0f));
+    }
+}
