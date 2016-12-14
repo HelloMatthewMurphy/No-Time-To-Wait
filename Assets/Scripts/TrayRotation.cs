e3c4e274f324e808722b4caa8df29856b9136965
HelloMatthewMurphy <hellomatthewmurphy@gmail.com>
2016-12-14 17:27:27 +0000

    Added Tray
    Tray balancing

---
+++ Assets/Scripts/TrayRotation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrayRotation : MonoBehaviour {
+
+    public GameObject bodyGameObject;
+    public GameObject positionOfTray;
+    public GameObject tray;
+    public Transform bodyTransform;
+    public float rotatePrecent;
+
+    // Use this for initialization
+    void Start () {
+        bodyGameObject = GameObject.FindGameObjectWithTag("Body");
+        tray = GameObject.FindGameObjectWithTag("Tray");
+        positionOfTray = GameObject.FindGameObjectWithTag("TrayPos");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float bodyRotation = bodyTransform.eulerAngles.z;
+        if (bodyRotation > 180)
+        {
+            bodyRotation = -(360 - bodyRotation);
+            //Debug.Log(bodyRotation);
+        }
+        else
+        {
+            bodyRotation = bodyRotation;
+            //Debug.Log(bodyRotation);
+        }
+        transform.eulerAngles = new Vector3 (0, 0, bodyRotation * rotatePrecent);
+        //if(tray.transform.rotation > 90)
+        tray.transform.position = positionOfTray.transform.position;
+    }
+}
