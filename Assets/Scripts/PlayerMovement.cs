28e6747db12f977a557a3d461da470cb2f96b512
worm00111 <vdrumsta@gmail.com>
2017-01-05 14:58:25 +0000

    V0.1.3
    When the player falls over, he automatically stands up after 2 seconds

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,33 +15,36 @@ public class PlayerMovement : MonoBehaviour {
 
     private float maxBodyAngle;
     private MouseBalance mouseBalanceScript;
-    private Vector2 respawnLocation;    // location where the player will respawn after falling down
+    private Vector2 legRespawnPosition;     // Position where the legs will respawn after falling down
     private bool fallingExecuted;
+    private Vector2 initBodyPosTrans;       // How much you need to add to leg position, to arrive at the initial body position
 
     // Use this for initialization
     void Start () {
         standing = true;
         rb = GetComponent<Rigidbody2D>();
-        maxBodyAngle = Mathf.Abs(bodyGameObject.GetComponent<HingeJoint2D>().limits.max);  // gets the positive angle at which the body falls over
+        maxBodyAngle = Mathf.Abs(bodyGameObject.GetComponent<HingeJoint2D>().limits.max);   // gets the positive angle at which the body falls over
         mouseBalanceScript = bodyGameObject.GetComponent<MouseBalance>();
+        initBodyPosTrans = bodyTransform.position - transform.position;                     // How much you need to add to leg position, to arrive at the initial body position
     }
     
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float bodyRotation = bodyTransform.eulerAngles.z;
-        if (bodyRotation > 180) // right side
+        float bodyRotation = bodyTransform.eulerAngles.z;   // Record the rotation of the body
+
+        if (bodyRotation > 180) // Right side tilting of the body
         {
             bodyRotation = 360 - bodyRotation;
         }
-        else                    // left side
+        else                    // Left side tilting of the body
         {
             bodyRotation = -(bodyRotation);
         }
 
         if (standing && enableFalling)
         {
-            standing = Mathf.Abs(bodyRotation) < maxBodyAngle;  // false if body is tilting beyond max body angle
+            standing = Mathf.Abs(bodyRotation) < maxBodyAngle;  // False if body is tilting beyond max body angle
         }
 
         if (standing)
@@ -50,32 +53,57 @@ public class PlayerMovement : MonoBehaviour {
 
             fallingExecuted = false;
         }
-        else if (!fallingExecuted)   // only executes once
+        else if (!fallingExecuted)   // Only executes once everytime the waiter falls over
         {
             fallOver();
             fallingExecuted = true;
-            //Invoke("standUp", 2f);
+            Invoke("standUp", 2f);
         }
     }
 
+    /* 
+     * Fall Over simulates the waiter tipping over and loosing control.
+     * The method achieves it by removing control from the player and enabling rotation on the legs,
+     * making it look like the waiter is falling over.
+     */
     private void fallOver()
     {
-        respawnLocation = transform.position;           // records the position where the player fell over
+        legRespawnPosition = transform.position;        // records the position where the player fell over
 
         mouseBalanceScript.setEnableRotation(false);    // removes rotation control
         rb.constraints = RigidbodyConstraints2D.None;   // lets the legs fall over like a ragdoll
         bodyGameObject.GetComponent<BoxCollider2D>().isTrigger = false; // turns the bodies hitbox into non-trigger meaning its collision is on
     }
 
+    /*  
+     * Stand Up returns the waiter to the location where he lost control.
+     * It also sets the legs and the body upright.
+     */
     private void standUp()
     {
-        transform.rotation = Quaternion.Euler(0, 0, 0);
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        transform.rotation = Quaternion.Euler(0, 0, 0);             // sets rotation to 0
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;     // locks rotation
+        transform.position = legRespawnPosition;
+
+        // Construct new body pos, so it's alligned with legs and has the correct distance from the legs
+        Vector2 newBodyPosition = legRespawnPosition;
+        newBodyPosition += initBodyPosTrans;
+
+        bodyTransform.rotation = Quaternion.Euler(0, 0, 0);         // sets body's rotation to 0
+        bodyTransform.position = newBodyPosition;
+
+        standing = true;
+        mouseBalanceScript.setEnableRotation(true);                    // Returns rotation control
+        bodyGameObject.GetComponent<BoxCollider2D>().isTrigger = true;  // turns the bodies hitbox into a trigger meaning its collision is off
     }
 
+    /*
+     * Purpose of applyMovement is to move the waiter left and right, 
+     * depending on how much he is leaning over
+     */
     private void applyMovement(float bodyRotation)
     {
-        rb.AddForce(new Vector2(bodyRotation / (maxBodyAngle + 2) * tiltSpeed, 0f)); // can't remember what + 2 is for. Too afraid to change it at this point
+        rb.AddForce(new Vector2(bodyRotation / (maxBodyAngle + 2) * tiltSpeed, 0f)); // + 2 for corrections
 
         Vector3 easeVelocity = rb.velocity;
         easeVelocity.y = rb.velocity.y;
@@ -99,5 +127,10 @@ public class PlayerMovement : MonoBehaviour {
         {
             rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
         }
+    }
+
+    public bool getStanding()
+    {
+        return standing;
     }
 }
